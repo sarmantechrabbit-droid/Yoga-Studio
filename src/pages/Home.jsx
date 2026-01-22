a8a1a408fa5e593bc80dbdd20627c5628276f5c5
sarman parmar <sarmantechrabbit@gmail.com>
2026-01-22 16:48:27 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from '../components/Header'
+import YogaHeroSection from '../components/sections/YogaHeroSection'
+import YogaAboutSection from '../components/sections/YogaAboutSection'
+import YogaServicesSection from '../components/sections/YogaServicesSection'
+import Specialities from '../components/sections/Specialities'
+import YogaEventsSection from '../components/sections/YogaEventsSection'
+import YogaMarquee from '../components/sections/YogaMarquee'
+import TestimonialsSection from '../components/sections/TestimonialsSection'
+import FaqSection from '../components/sections/FaqSection'
+import YogaMoments from '../components/sections/YogaMoments'
+import Footer from '../components/sections/Footer'
+
+function Home() {
+  return (
+    <div>
+       <Header />
+     <YogaHeroSection />
+     <YogaAboutSection />
+     <YogaServicesSection />
+     <Specialities />
+     <YogaEventsSection />
+     <YogaMarquee />
+     <TestimonialsSection />
+     <FaqSection />
+     <YogaMoments />
+     <Footer />   
+    </div>
+  )
+}
+
+export default Home
